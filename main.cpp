a1b7805fc5eeb002cdc14c67fa0b74be3b6b56a5
sonenagisa <csna20061@g.nihon-u.ac.jp>
2024-06-28 04:01:57 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+RawSerial gs(USBTX,USBRX,9600);
+DigitalOut cond(LED1);
+Timer sattime;
+
+int rcmd = 0, cmdflag = 0;
+void commandget(){
+    rcmd = gs.getc();
+    cmdflag = 1;
+}
+
+void receive(int rcmd, int cmdflag){
+    gs.attach(commandget,Serial::RxIrq);
+}
+
+void initialize(){
+    rcmd = 0;
+    cmdflag = 0;
+}
 
 int main() {
+  sattime.start();
+  receive(rcmd,cmdflag);
+  float vol = 3.7; //demo data
   while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
+    gs.printf("hepta-sat: time = %f vol = %f\r\n",sattime.read(),vol);
+    if(cmdflag == 1){
+        if(rcmd == 'A'){
+            gs.printf("Hello World!!\r\n");
+        }
+        initialize();
+    }
+    wait(1.0);
   }
+  sattime.stop();
 }
